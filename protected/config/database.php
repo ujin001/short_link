fa0d939fc9a63ddae1722287993ac7aee41bdc23
Pavel Bil <bilpavel@gmail.com>
2015-01-24 15:40:12 +0700

    init

---
+++ protected/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+	// uncomment the following lines to use a MySQL database
+	'connectionString' => 'mysql:host=localhost;dbname=short_link',
+	'emulatePrepare' => true,
+	'username' => 'short_link',
+	'password' => '1q2w3e',
+	'charset' => 'utf8',
+
+);
---
+++ protected/models/ShortUrlForm.php
@@ -0,0 +1,11 @@
+<?php
+
+class ShortUrl extends CFormModel {
+	public $url;
+	public $short_url;
+
+	private function generate() {
+		$short_url = '';
+		return $short_url;
+	}
+}
